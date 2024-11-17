e6f6eefb5468706ccfb498cc6bc5c30791f380f1
aryanyaclav <aycool19@gmail.com>
2024-11-17 11:48:50 +0530

    added html report generation functionality

--- tests_suite.py
+++ tests_suite.py
@@ -1,4 +1,5 @@
 import unittest
+import HtmlTestRunner
 
 test_modules = ["tests.login_test", 
                 "tests.add_to_cart"]
@@ -13,4 +14,15 @@ for module in test_modules:
     except (ImportError, AttributeError):
         suite.addTest(unittest.defaultTestLoader.loadTestsFromName(module))
 
-unittest.TextTestRunner().run(suite)
+# Configure the HTMLTestRunner
+runner = HtmlTestRunner.HTMLTestRunner(
+    output='test_reports',      # Directory to save the report
+    report_name='TestReport',   # Custom report name
+    combine_reports=True,       # Combine all reports into a single file
+    add_timestamp=True          # Append timestamp to the report name
+)
+
+# Run the test suite
+runner.run(suite)
+
+#unittest.TextTestRunner().run(suite)
